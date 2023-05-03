b987ebbfe5053c57c8bdf8b0bdd31f8370ad3e84
carveyh <carvey.hor@gmail.com>
2023-05-03 17:39:34 -0400

    Phase 1 #followUser #unfollowUser

--- practice-for-ch-js-ajax-twitter-long-practice-main/app/javascript/util/api.js
+++ practice-for-ch-js-ajax-twitter-long-practice-main/app/javascript/util/api.js
@@ -4,9 +4,28 @@ async function customFetch(url, options = {}) {
   options.headers = {
     // Your code here
     ...options.headers
+    // food: "lots:"
   };
-
+  console.log("before error?")
   return await fetch(url, options);
 }
 
 // export const foo = "bar";
+
+export function followUser(id) {
+  console.log("follwo user?")
+  return customFetch(`/users/${id}/follow`, {
+    method: "POST"
+    // headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' }
+  });
+}
+
+export function unFollowUser(id) {
+  // console.log("follow the trail")
+  return customFetch(`/users/${id}/follow`, {
+    method: "DELETE"
+    
+    // headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' }
+  });
+}
+
